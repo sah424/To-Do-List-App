ec543e162f62a46da883dff8713b2c6160999b99
sah424 <sahinparvezsp1432@gmail.com>
2025-06-10 23:29:03 +0530

    first commit

--- src/classes/AppFrame.java
+++ src/classes/AppFrame.java
@@ -7,21 +7,24 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 
-//It is the app framework where it connets all the other classes to one frame
+//It is the app framework where it connects all the other classes to one frame
 
 //Here we add some functionality
 public class AppFrame extends JFrame {
 
+    //Variables
+    //The app gonna split up into 3 things
     private TitleBar title;
     private List list;
     private  ButtonPanel btnPanel;
 
-    //Getter Methods
+    //Getter Methods to make them do things(add some functionality)
     private JButton addTask;
     private JButton clear;
 
     //Constructor
     AppFrame() {
+        //App Frame Design
         this.setSize(400,700);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
@@ -29,23 +32,28 @@ public class AppFrame extends JFrame {
         title = new TitleBar();
         list = new List();
         btnPanel = new ButtonPanel();
+        //these are the functions which connect the functionality(add) after design the app in each classes
         this.add(title, BorderLayout.NORTH);
         this.add(btnPanel, BorderLayout.SOUTH);
-
         this.add(list,BorderLayout.CENTER);
 
+        //these are the functionality for Task
+        //Adding task
         addTask = btnPanel.getAddTask();
+        //Clear the completed task
         clear = btnPanel.getClear();
 
-
+        //Function created
         addListeners();
 
     }
 
+    //Made this so when we click on "Add Task" it's going to add a new task
     public void addListeners(){
         addTask.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
+                //making new task
                 Task task = new Task();
                 list.add(task);
                 //1,2,3,4 in the ToDoList
--- src/classes/TitleBar.java
+++ src/classes/TitleBar.java
@@ -9,12 +9,14 @@ import java.awt.*;
 public class TitleBar extends JPanel {
     //Constructor
     TitleBar(){
+        //Title Box "TO Do List" Size,Colour,Position
         this.setPreferredSize(new Dimension(400,80));
 
         this.setBackground(new Color(255, 238, 88)); // bright yellow
 
 
         JLabel titleText = new JLabel("To Do List");
+        //titleText is using because it define Text's functions
         titleText.setPreferredSize (new Dimension(200,80));
         titleText.setFont(new Font("Sans-serif", Font.BOLD,20));
         titleText.setHorizontalAlignment(JLabel.CENTER);
